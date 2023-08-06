36b27a09ed694d2571e12e0dacd92fb76c799fa9
Aaditya Pratap Singh <137618627+aaditwocode@users.noreply.github.com>
2023-08-06 18:02:27 +0530

    Add files via upload

---
+++ 11.c
@@ -0,0 +1,30 @@
+// #include<stdio.h>
+
+//     int main(){
+//         for (int i = 1; i < 11; i++)
+//         {
+//             printf("%d\n" , i);
+//         }
+        
+//     return 0;
+// } 
+// #include<stdio.h>
+
+//     int main(){
+//         for (int i = 10; i>= 1; i--)
+//         {
+//             printf("%d\n" , i);
+//         }
+        
+//     return 0;
+// } 
+#include<stdio.h>
+
+    int main(){
+        for (int i = 1; i <= 11; i++)
+        {
+            printf("%d\n" , 11-i);
+        }
+        
+    return 0;
+} 
