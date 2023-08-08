229523899aa0ca7f8e231c030af13f72dd57a2d4
Aaditya Pratap Singh <137618627+aaditwocode@users.noreply.github.com>
2023-08-08 23:29:46 +0530

    Add files via upload

---
+++ 19.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+    int main(){
+        int x=1,y=10;
+        while (x<y)
+        {
+           x++;
+           y--;
+        
+        }
+        printf("%d  %d" , x, j);
+    return 0;
+}
